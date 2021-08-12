cd01c755b5bbb8b384ae5818dcf27e4076311cab
Mike Friesen <mfriesen@gmail.com>
2021-08-11 22:15:30 -0500

    Added support for getting email template from S3 Bucket

--- custom_message/app.js
+++ custom_message/app.js
@@ -5,19 +5,20 @@
  */
 const AWS = require('aws-sdk');
 var ssm = new AWS.SSM();
+var s3 = new AWS.S3();
 var defaults = {};
 defaults["CustomMessage_SignUp"] = {};
 defaults["CustomMessage_ForgotPassword"] = {};
 defaults["CustomMessage_AdminCreateUser"] = {};
 
 defaults["CustomMessage_SignUp"]["Subject"] = "Your Verification Link";
-defaults["CustomMessage_SignUp"]["Message"] = "Thank you for signing up. ${link}";
+defaults["CustomMessage_SignUp"]["Message"] = "Thank you for signing up. <a href=\"${link}\" target=\"_blank\">Click this link to verify</a>";
 
 defaults["CustomMessage_ForgotPassword"]["Subject"] = "Your Reset Password link";
-defaults["CustomMessage_ForgotPassword"]["Message"] = "You have requested a password reset. ${link}";
+defaults["CustomMessage_ForgotPassword"]["Message"] = "You have requested a password reset. <a href=\"${link}\" target=\"_blank\">Click this link to Reset Password</a>";
 
 defaults["CustomMessage_AdminCreateUser"]["Subject"] = "Your Account has been Created";
-defaults["CustomMessage_AdminCreateUser"]["Message"] = "Your account has been created. ${link}";
+defaults["CustomMessage_AdminCreateUser"]["Message"] = "Your account has been created. <a href=\"${link}\" target=\"_blank\">Click this link to finalize your account.</a>";
 
 exports.lambdaHandler = async (event, context, callback) => {
 
@@ -28,86 +29,127 @@ exports.lambdaHandler = async (event, context, callback) => {
             region: 'us-east-1',
             endpoint: 'http://localhost:4566'
         });
+
+        s3 = new AWS.S3({
+            endpoint: 'http://localhost:4566',
+            s3ForcePathStyle: true
+        });
     }
 
     return getParameter(event).then((data) => {
-            
-        let urlCache = data.Parameters.find(o => o.Name.endsWith("CognitoHttpApiUrl"));
-        var subject = data.Parameters.find(o => o.Name.endsWith("Subject"));
-        var message = data.Parameters.find(o => o.Name.endsWith("Message"));
 
-        if (subject === undefined) {
-            subject = defaults[event.triggerSource]["Subject"];
-        } else {
-            subject = subject.Value;
-        }
+        return getS3Files(event).then((s3data) => {
 
-        if (message === undefined) {
-            message = defaults[event.triggerSource]["Message"];
-        } else {
-            message = message.Value;
-        }
+            var message = s3data;
 
-        if (event.triggerSource === "CustomMessage_SignUp") {
-    
-            const { codeParameter } = event.request;
-            const { userName, region } = event;
-            const { clientId } = event.callerContext;
-            const { email } = event.request.userAttributes;
+            let urlCache = data.Parameters.find(o => o.Name.endsWith("CognitoHttpApiUrl"));
+            var subject = data.Parameters.find(o => o.Name.endsWith("Subject"));
 
-            const userStatus = event.request.userAttributes['cognito:user_status'];
-    
-            const link = `<a href="${urlCache.Value}/confirmSignUp?userStatus=${userStatus}&code=${codeParameter}&username=${userName}&clientId=${clientId}&region=${region}&email=${email}" target="_blank">Click this link to verify</a>`;
+            // var message = data.Parameters.find(o => o.Name.endsWith("Message"));
+            // var message = 
 
-            event.response.emailSubject = subject;
-            event.response.emailMessage = processMessage(event.request.userAttributes, message, link);
+            if (subject === undefined) {
+                subject = defaults[event.triggerSource]["Subject"];
+            } else {
+                subject = subject.Value;
+            }
 
-        } else if (event.triggerSource === "CustomMessage_ForgotPassword") {
+            if (message === undefined || message.length == 0) {
+                message = defaults[event.triggerSource]["Message"];
+            }
 
-            const { codeParameter } = event.request;
-            const { userName, region } = event;
-            const { clientId } = event.callerContext;
-            const { email } = event.request.userAttributes;
-            const userStatus = event.request.userAttributes['cognito:user_status'];
-    
-            const link = `<a href="${process.env.REDIRECT_URI}/lostpassword?userStatus=${userStatus}&code=${codeParameter}&username=${userName}&clientId=${clientId}&region=${region}&email=${email}" target="_blank">Click this link to Reset Password</a>`;
-
-            event.response.emailSubject = subject;
-            event.response.emailMessage = processMessage(event.request.userAttributes, message, link);
-
-        } else if (event.triggerSource == "CustomMessage_AdminCreateUser") {
-    
-            const { codeParameter, usernameParameter } = event.request;
-            const { region, userName } = event;
-            const { clientId } = event.callerContext;
-            const { email } = event.request.userAttributes;
-
-            const userStatus = event.request.userAttributes['cognito:user_status'];
-    
-            const link = `<a href="${urlCache.Value}/confirmRegistration?userStatus=${userStatus}&code=${codeParameter}&username=${userName}&clientId=${clientId}&region=${region}&email=${usernameParameter}" target="_blank">Click this link to finalize your account.</a>`;
-
-            event.response.emailSubject = subject;
-            event.response.emailMessage = processMessage(event.request.userAttributes, message, link);
-        }
-
-        console.log(JSON.stringify(event.response));
+            if (event.triggerSource === "CustomMessage_SignUp") {
         
-        callback(null, event);
-        return event;
+                const { codeParameter } = event.request;
+                const { userName, region } = event;
+                const { clientId } = event.callerContext;
+                const { email } = event.request.userAttributes;
+
+                const userStatus = event.request.userAttributes['cognito:user_status'];
+        
+                const link = `${urlCache.Value}/confirmSignUp?userStatus=${userStatus}&code=${codeParameter}&username=${userName}&clientId=${clientId}&region=${region}&email=${email}`;
+
+                event.response.emailSubject = subject;
+                event.response.emailMessage = processMessage(event.request.userAttributes, message, link);
+
+            } else if (event.triggerSource === "CustomMessage_ForgotPassword") {
+
+                const { codeParameter } = event.request;
+                const { userName, region } = event;
+                const { clientId } = event.callerContext;
+                const { email } = event.request.userAttributes;
+                
+                const userStatus = event.request.userAttributes['cognito:user_status'];
+        
+                const link = `${process.env.REDIRECT_URI}/lostpassword?userStatus=${userStatus}&code=${codeParameter}&username=${userName}&clientId=${clientId}&region=${region}&email=${email}`;
+
+                event.response.emailSubject = subject;
+                event.response.emailMessage = processMessage(event.request.userAttributes, message, link);
+
+            } else if (event.triggerSource == "CustomMessage_AdminCreateUser") {
+        
+                const { codeParameter, usernameParameter } = event.request;
+                const { region, userName } = event;
+                const { clientId } = event.callerContext;
+                const { email } = event.request.userAttributes;
+
+                const userStatus = event.request.userAttributes['cognito:user_status'];
+        
+                const link = `${urlCache.Value}/confirmRegistration?userStatus=${userStatus}&code=${codeParameter}&username=${userName}&clientId=${clientId}&region=${region}&email=${usernameParameter}`;
+
+                event.response.emailSubject = subject;
+                event.response.emailMessage = processMessage(event.request.userAttributes, message, link);
+            }
+
+            console.log(JSON.stringify(event.response));
+            
+            callback(null, event);
+            return event;
+        });
 
     }).catch((err) => {
-        console.log("ERRO!: " + err);
+        console.log(err);
     });
 };
 
 function processMessage(userAttributes, message, link) {
     const { email } = userAttributes;
-    return message.replace("${link}", link).replace("${email}", email);
+    let emailLocal = email.substring(0, email.indexOf("@"));
+    return message.replace("${link}", link).replace("${email}", email).replace("${emailLocal}", emailLocal);
 }
 
 async function getParameter(event) {
     return ssm.getParameters({Names:["/formkiq/cognito/" + process.env.DOMAIN + "/CognitoHttpApiUrl", 
-        "/formkiq/cognito/" + process.env.DOMAIN + "/" + event.triggerSource + "/Subject",        
-        "/formkiq/cognito/" + process.env.DOMAIN + "/" + event.triggerSource + "/Message"
+        "/formkiq/cognito/" + process.env.DOMAIN + "/" + event.triggerSource + "/Subject"
     ]}).promise();
+}
+
+async function getS3Files(event) {
+    var params = {
+        Bucket: process.env.S3_BUCKET,
+        Key: "formkiq/cognito/" + process.env.DOMAIN + "/CustomMessage_AdminCreateUser/Message"
+    };
+
+    var s3Stream = s3.getObject(params).createReadStream();
+
+    return stream2buffer(s3Stream).then((buff) => {
+        let s = buff.toString('utf8');
+        return s;
+    }).catch((err) => {
+        console.log(err);
+        return "";
+    });
+}
+
+function stream2buffer(stream) {
+
+  return new Promise((resolve, reject) => {
+      
+    const _buf = [];
+
+    stream.on("data", (chunk) => _buf.push(chunk));
+    stream.on("end", () => resolve(Buffer.concat(_buf)));
+    stream.on("error", (err) => reject(err));
+
+  });
 }
